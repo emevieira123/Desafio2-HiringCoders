82c6dfa136a2082194fe22427379a930cfa4f624
Emerson Vieira <emevieira@gmail.com>
2021-07-25 19:06:12 -0400

    alteracao da url da api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/produtos'
+  baseURL: 'https://my-json-server.typicode.com/emevieira123/FakeApiDesafio2/produtos'
 })
 
 export default api;
